356ccf04afd22f6ff8504352aceea94c40b34e2a
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-09-06 08:54:04 -0400

    yuh

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,14 +1,22 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        /*BMI= Kg/M*M
+        M = inch*(0.0254meters/1inch)
+        inch = M*(1inch/0.0254meters)
+         */
 
+        Scanner keyboard;
+        String userHeight;
+        String userWeight;
+      keyboard= new Scanner(System.in);
 
-
-
-
+      System.out.println();
 
 
 
